7e916e21ad3e74922886ac81de49f5466bdeccb2
abelited <219223304+abelited@users.noreply.github.com>
2026-05-05 08:16:05 -0400

    Fix vite config and rebuild with relative paths

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Set base to './' for relative paths
+  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
-
-
